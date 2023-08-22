58739d60ea5e4c448c7aff36cf9b3ec27bb8b275
Kasper Kondzielski <kasper.kondzielski@siriusxm.com>
2023-08-22 18:29:52 +0200

    Add comments

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -14,14 +14,17 @@ case class B(foo: Set[Baz])
 object Main {
 
   def here(baz: Baz) = A(
+    // doesn't work here
     foo = baz.methodA(_.isEmpty())
   )
 
   def here2(baz: Baz) = B(
+    // works here
     foo = baz.methodA(_.isEmpty())
   )
 
   def here3(baz: Baz) = A(
+    // works here
     baz.methodA(_.isEmpty())
   )
 
