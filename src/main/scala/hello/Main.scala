9f7d8f7c76c4c29bd668ae4497b200e803391862
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-11-11 19:24:36 +0100

    Add comments

--- src/main/scala/hello/Main.scala
+++ src/main/scala/hello/Main.scala
@@ -3,11 +3,14 @@ package hello
 import hello.another.Backend
 
 object Main {
+  // go to definition from here doesn't work
   private val dummyBackend = new Backend {
 
     def get(key: String): List[String] = List.empty
   }
 
+  // go to definition from function argument works
   def main(args: Backend) = {}
+  
 
 }
